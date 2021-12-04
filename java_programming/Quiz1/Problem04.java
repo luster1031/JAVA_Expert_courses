a4b960144a223b63f4f59313f4cf7bd28f158fe9
luster1031 <61460836+luster1031@users.noreply.github.com>
2021-12-04 16:29:01 +0900

    Problem04.java

---
+++ java_programming/Quiz1/Problem04.java
@@ -0,0 +1,12 @@
+package Quiz;
+
+public class Problem04 {
+	public static void main(String [] args) {
+		for(int i = 1; i<11; i++) {
+			for(int j =i; j<i+10;j++) {
+				System.out.print(j + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
