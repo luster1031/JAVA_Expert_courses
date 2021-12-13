a0c773ab41c9aeeea3b9362ffc07ba7d5c20f8cf
luster1031 <61460836+luster1031@users.noreply.github.com>
2021-12-14 03:04:21 +0900

    bubble, quick, insert sort && binary search

---
+++ Data-Structure/Sort_and_Serach.java
@@ -0,0 +1,111 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Sort {
+	private int [] random_num;
+	public Sort() {
+		random_num = new int[101];
+		random_num = random_array(random_num);
+	}
+	public int[] random_array(int [] random) {
+		int number =0;
+		Random r = new Random();
+		for(int i= 0; i<random_num.length;i++) {
+			random[i] = (int)(r.nextInt(100)+1);
+		}
+		return random;
+	}
+	//버블 정렬
+	public void bubble_sort(int [] number) {
+		for(int i = number.length-1; i>=0; i--) {
+			for(int  exchange= 0,tmp =0; exchange<i;exchange++) {
+				if(number[exchange] > number[exchange+1]) {
+					swap(number, exchange, exchange+1);
+				}
+			}
+		}
+		System.out.println("bubble sort =>  " + Arrays.toString(number));
+	}
+	//삽입 정렬
+	public void insert_sort(int [] number) {
+		for(int i = 1; i<number.length; i++) {
+			for(int index = i-1, back_index = i; index >=0 && number[index] > number[back_index]; index--,back_index--) {
+				swap(number, index, back_index);
+			}
+		}
+		System.out.println("inset sort =>   " + Arrays.toString(number));
+	}
+	//퀵 정렬
+	public int [] quick_sort(int [] number,int start, int end) {
+		int part2 = partition(number, start,end);
+	
+		if(start < part2 -1) {
+			quick_sort(number, start, part2-1);
+		}
+		if(part2 < end) {
+			quick_sort(number, part2, end);
+		}
+		return number;
+	}
+	public int partition(int [] number, int start, int end) {
+		int pivot = number[(start+end)/2];	//중간 값
+		while(start <= end) {
+			while(number[start]<pivot)start++;
+			while(number[end] > pivot) end--;
+			if(start <= end) {
+				swap(number, start, end);
+				start ++;
+				end--;
+			}
+		}
+		return start;
+	}
+	public void swap(int [] number, int first, int second) {
+		int tmp = number[first];
+		number[first] = number[second];
+		number[second] = tmp;
+	}
+	//	이진 탐색
+	public int binary_search(int [] number,int target) {
+		int mid =0;
+		int end = number.length;
+		int start = 0;
+		
+		while(start <= end) {
+			mid = (end + start)/2;
+			if(number[mid] > target) {
+				end = mid-1;
+			}else if(number[mid] < target) {
+				start = mid+1;
+			}
+			else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		Sort s = new Sort();
+		System.out.println("origin array : "+ Arrays.toString(s.random_num));
+		//	버블 정렬
+		s.bubble_sort(s.random_num);
+		
+		//	삽입 정렬
+		s.insert_sort(s.random_num);
+		
+		//	퀵 정렬
+		int [] quick_sort_array = s.quick_sort(s.random_num, 0, s.random_num.length-1);
+		System.out.println("quick sort =>   " + Arrays.toString(quick_sort_array));
+		
+		//	이진 탐색
+		int index = s.binary_search(quick_sort_array, 60);
+		if(index != -1) {
+			System.out.printf("%d은 %d번째 방에 있습니다.", 60,index );
+		}else {
+			System.out.printf("%d는 현재 array에 없습니다.",60);
+		}
+		
+	}
+}
