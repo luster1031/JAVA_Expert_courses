b20114cbfe8425352d152a7a66004e34b6b30257
luster47 <luster47@naver.com>
2021-12-27 17:13:52 +0900

    tomcat practice- html, servelt

---
+++ src/main/java/FirstServelt.java
@@ -0,0 +1,27 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first")
+public class FirstServelt extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>First Servlet 수행 완료 </h1>");
+		out.print("<hr>");
+		out.print("<h2>요청 방식 : " + request.getMethod() + "</h2>");	
+		out.print("<h2>반가워요...: " + request.getParameter("name") + "님!!</h2>");	
+		out.close();
+		System.out.println("서블릿에서의 표준 출력은 어디로 나가나?");
+	}
+
+}
---
+++ src/main/webapp/jspsrc/first.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>2021.12.27 월요일 jsp</title>
+</head>
+<body>
+
+	<h1>JSP 맛보기 예제</h1>
+	<hr>
+	<%=new java.util.Date()%>
+
+
+</body>
+</html>
