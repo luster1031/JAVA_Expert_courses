4a8262539e03e7aac5d8158bc09179d4107e250f
Lim-HN <61460836+luster1031@users.noreply.github.com>
2022-01-04 14:13:19 +0900

    mebmerVO

---
+++ ServletJSP/practice10/MemberVO.java
@@ -0,0 +1,32 @@
+package vo;
+
+public class MemberVO {
+	private String name;
+	private String number;
+	private String account;
+	private String passwd;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+}
