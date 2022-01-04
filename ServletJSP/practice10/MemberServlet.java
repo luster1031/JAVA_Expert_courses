5f4815eb6e191ac11bb38d33beb59685fb6dc03a
Lim-HN <61460836+luster1031@users.noreply.github.com>
2022-01-04 14:13:39 +0900

    memberServlet

---
+++ ServletJSP/practice10/MemberServlet.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vo.MemberVO;
+
+@WebServlet("/memberinfo")
+public class MemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		MemberVO vo = new MemberVO();
+		ArrayList<String> info = new ArrayList<>(Arrays.asList("myname","mynumber","myacc","mypasswd"));
+		String str = "";
+		
+		for(int i = 0; i<info.size(); i++) {
+			str = request.getParameter(info.get(i));
+			if(str == null || str.equals(""))
+				info.set(i, "없음");
+			else
+				info.set(i, str);
+		}
+		vo.setName(info.get(0));		
+		vo.setNumber(info.get(1));
+		vo.setAccount(info.get(2));
+		vo.setPasswd(info.get(3));
+		
+		request.setAttribute("memberinfo", vo); 
+		request.getRequestDispatcher("/jspsrc/memberView.jsp").forward(request, response);
+	}
+
+}
